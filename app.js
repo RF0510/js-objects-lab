19a2ac7695ecd79e0f4d436e099eb34b9cb09110
RF0510 <rosevelt.foushee@yahoo.com>
2024-07-06 18:19:01 -0400

    Exercise 15

--- app.js
+++ app.js
@@ -16,51 +16,51 @@ const game = {
         { name: "pokeball", quantity: 8 },
         { name: "rare candy", quantity: 99 },
     ],
-    /*
-Exercise 14
-1. Add a `partyCount` method to `game` that counts the number of Pokémon in your party.
+/*
+Exercise 15
+1. Now, complete gyms with a difficulty below 8. Reflect on how this is similar to or different from the previous gym exercises.
+(change the value of `complete` in the qualifying objects from false to true).
 
-This method should:
-  - Not accept any arguments.
-  - Count the number of Pokemon in the party.
-  - return the found number of Pokemon in the party.
-
-Solve Exercise 14 here:
+Solve Exercise 15 here:
 */
-// UsedChatGPT
-    catchPokemon: function (pokemonObj) {
-        this.party.push(pokemonObj);
-        const pokeballItem = this.items.find(item => item.name === "pokeball");
-        if (pokeballItem) { pokeballItem.quantity--; }
-    },
-    completeGymsBelowDifficulty: function(difficultyThreshold) {
-        this.gyms.forEach(gym => {
-            if (gym.difficulty < difficultyThreshold) {
-                gym.completed = true;
-            }
-        });
-    },
-    gymStatus: function() {
-        const gymTally = {
-            completed: 0,
-            incomplete: 0
-        };
+catchPokemon: function(pokemonObj) {
+    this.party.push(pokemonObj);
 
-        this.gyms.forEach(gym => {
-            if (gym.completed) {
-                gymTally.completed++;
-            } else {
-                gymTally.incomplete++;
-            }
-        });
-
-        console.log("Gym Tally:", gymTally);
-    },
-    partyCount: function() {
-        return this.party.length;
+    const pokeballItem = this.items.find(item => item.name === "pokeball");
+    if (pokeballItem) {
+        pokeballItem.quantity--;
     }
+},
+completeGymsBelowDifficulty: function(difficultyThreshold) {
+    this.gyms.forEach(gym => {
+        if (gym.difficulty < difficultyThreshold) {
+            gym.completed = true;
+        }
+    });
+},
+gymStatus: function() {
+    const gymTally = {
+        completed: 0,
+        incomplete: 0
+    };
+
+    this.gyms.forEach(gym => {
+        if (gym.completed) {
+            gymTally.completed++;
+        } else {
+            gymTally.incomplete++;
+        }
+    });
+
+    console.log("Gym Tally:", gymTally);
+},
+partyCount: function() {
+    return this.party.length;
+}
 };
 
-console.log("Number of Pokemon in the party:", game.partyCount());
+game.completeGymsBelowDifficulty(8);
 
+
+game.gymStatus();
 
