55272dccd0f46aed8f22577e1aaae50c3046b6f5
RF0510 <rosevelt.foushee@yahoo.com>
2024-07-06 11:01:08 -0400

    Exercise 8

--- app.js
+++ app.js
@@ -17,46 +17,18 @@ const game = {
       { name: "rare candy", quantity: 99 },
     ],
   }
-  /*
-Exercise 7
-1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
-2. How would you replace the current starter Pokémon in your party with its evolved form?
 
-Hint: 
-  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
-  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
-  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
-  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
 
-More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
-
-
-Solve Exercise 7 here:
+Solve Exercise 8 here:
 */
 
-
-// Used ChatGPT
-
-const currentStarter = game.party.find(pokemon => pokemon.starter);
-
-if (currentStarter) {
-    
-    const evolvedPokemon = pokemon.find(p => p.number === currentStarter.number + 1);
-
-    if (evolvedPokemon) {
-        
-        const index = game.party.indexOf(currentStarter);
-        if (index !== -1) {
-            game.party.splice(index, 1, evolvedPokemon);
-        }
-    } else {
-        console.log("Evolved form not found.");
-    }
-} else {
-    console.log("No starter Pokémon found in the party.");
-}
-
-console.log(game);
+game.party.forEach(pokemon => {
+    console.log(pokemon.name);
+});
 
 
 
